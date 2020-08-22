b949d56b7782e9e3528415cc544d2ed07b433adb
pan <panakeia@protonmail.com>
2020-08-22 22:56:56 +0200

    Add nix flake for building Slippi

---
+++ flake.nix
@@ -0,0 +1,108 @@
+{
+  description = "Slippi overlay";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    slippiDesktopApp = {
+      url = "github:project-slippi/slippi-desktop-app";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, slippiDesktopApp }:
+    let
+      systems = [ "x86_64-linux" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+
+      slippi = playbackSlippi:
+        (forAllSystems (system:
+          with import nixpkgs { inherit system; };
+          stdenv.mkDerivation rec {
+            pname = "slippi-ishiiruka";
+            version = "2.2.1";
+            name = "${pname}-${version}-${
+                if playbackSlippi then "playback" else "netplay"
+              }";
+            src = builtins.path {
+              path = ./.;
+              inherit name;
+            };
+
+            cc = gcc;
+
+            enableParallelBuilding = true;
+            outputs = [ "out" ];
+            makeFlags = [ "VERSION=us" "-s" "VERBOSE=1" ];
+            hardeningDisable = [ "format" ];
+
+            cmakeFlags = [
+              "-DLINUX_LOCAL_DEV=true"
+              "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${glib.out}/lib/glib-2.0/include"
+              "-DGTK2_GDKCONFIG_INCLUDE_DIR=${gtk2.out}/lib/gtk-2.0/include"
+              "-DGTK2_INCLUDE_DIRS=${gtk2.out}/lib/gtk-2.0"
+              "-DENABLE_LTO=True"
+            ] ++ lib.optional (playbackSlippi) "-DIS_PLAYBACK=true";
+
+            postBuild = with lib;
+              optionalString playbackSlippi ''
+                rm -rf ../Data/Sys/GameSettings
+                cp -r "${slippiDesktopApp}/app/dolphin-dev/overwrite/Sys/GameSettings" ../Data/Sys
+              '' + ''
+                touch Binaries/portable.txt
+                cp -r -n ../Data/Sys/ Binaries/
+                cp -r Binaries/ $out
+                mkdir -p $out/bin
+              '';
+
+            installPhase = if playbackSlippi then ''
+              ln -s $out/dolphin-emu $out/bin/slippi-playback
+            '' else ''
+              ln -s $out/dolphin-emu $out/bin/slippi-netplay
+            '';
+
+            nativeBuildInputs = [ pkgconfig cmake ];
+            buildInputs = [
+              mesa_drivers
+              mesa_glu
+              mesa
+              pkgconfig
+              bluez
+              ffmpeg
+              libao
+              libGLU
+              gtk2
+              gtk3
+              glib
+              gettext
+              xorg.libpthreadstubs
+              xorg.libXrandr
+              xorg.libXext
+              xorg.libX11
+              xorg.libSM
+              readline
+              openal
+              libevdev
+              xorg.libXdmcp
+              portaudio
+              libusb
+              libpulseaudio
+              libudev
+              gnumake
+              wxGTK31
+              gdk-pixbuf
+              soundtouch
+              miniupnpc
+              mbedtls
+              curl
+              lzo
+              sfml
+              enet
+              xdg_utils
+              hidapi
+            ];
+          }));
+    in {
+      defaultPackage = slippi false;
+      playback = slippi true;
+    };
+}
